852ca4b316db9845816775aca1c031062ca1924d
Brian Shih <brianshihstart@gmail.com>
2023-11-05 11:13:47 +0800

    command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::{ffi::CString, path::PathBuf};
+
+use clap::{Parser, Subcommand};
+
+use crate::params::ContainerParams;
+mod params;
+
+#[derive(Parser)]
+struct Cli {
+    /// Command to execute
+    command: String,
+
+    /// Optional pid for child process
+    #[arg(short, long)]
+    pid: Option<u32>,
+
+    /// Memory limit (megabytes)
+    #[arg(short, long)]
+    memory: Option<u32>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let split_command = cli.command.split(" ").collect::<Vec<&str>>();
+    assert!(split_command.len() > 0);
+    let params = ContainerParams {
+        pid: 0,
+        exec_path: CString::new(split_command[0]).unwrap(),
+        args: split_command
+            .iter()
+            .map(|c| CString::new(*c).unwrap())
+            .collect(),
+        memory: Some(10),
+    };
+    println!("Params: {:?}", params);
 }
